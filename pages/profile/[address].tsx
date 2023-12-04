82e989cd601ba15e7d3267076bc0fc0f22dcacdb
Han-Enc <h01062598017@gmail.com>
2023-12-04 12:09:15 +0900

    third

--- pages/profile/[address].tsx
+++ pages/profile/[address].tsx
@@ -23,8 +23,8 @@ export default function ProfilePage() {
   );
   console.log(ownedNfts);
   return (
-    <Container maxW={'1200px'} p={5}>
-      <Heading color={'slateblue'}>{'Owned NFT(s)'}</Heading>
+    <Container maxW={'1200px'} p={5} bg="yellow.100">
+      <Heading>{'Owned NFT(s)'}</Heading>
       <Text>Browse and manage your NFTs from this collection.</Text>
       <NFTGrid
         data={ownedNfts}
